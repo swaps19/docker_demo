092921c59a3be9ad2baa067bfcd786da28cc753c
swaps19 <swapnil.shende@cuelogic.com>
2019-08-02 21:26:30 +0530

    restart container bugfix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
+echo 'starting the server'
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f /docker_demo/tmp/pids/server.pid
 
 # Start the main process
 bundle exec rails s -b 0.0.0.0
